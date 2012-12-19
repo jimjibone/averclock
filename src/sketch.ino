1c13387f9e1d408a77366ce9dfbc9d0aa5dcead1
Callan Bryant <callan.bryant@gmail.com>
2012-12-19 17:36:30 +0000

    compiling, scheduler, timers

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,37 +1,69 @@
+// TODO general scheduler addTask(pointer,interval,offset)
+// and better with a queue and priorities?
+
 #include "current_time.h"
 
-
-// tick interval in ms
+// tick interval in ms, 1-20 recommended, 260 max
 #define TICK 20
 
 // 1 second intervals to update clock
 #define HEARTBEAT_PERIOD (1000/TICK)
 
-int heartbeat_count = 0;
+volatile int heartbeat_count = 0;
 
 
 typedef struct {
-	char hour    = HOURS;
-	char minute  = MINUTES;
-	char seconds = SECONDS;
-} time;
+	char hours;
+	char minutes;
+	char seconds;
+} elapsed;
 
-volatile time time;
+// initialise with compile-time time
+elapsed time = {HOURS,MINUTES,SECONDS};
 
-void ISR (void);
+// tasks
 void inc_time (void);
+void update_display (void);
 
 
-void setup() {
-	// TODO set up timer to call ISR every TICK ms
+void setup () {
+	// enable interrupts (match A, too)
+	noInterrupts();
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TCNT1  = 0;
+
+	// set up timer1 @ 16MHz to call ISR every TICK ms
+	// will clobber servo lib and pwm 9 and 10
+
+	// TODO: A and B or what????
+
+	// SYS clock @ 16Mhz, Prescaler at 64, 16 bit timer1
+	// so 250 per ms
+	OCR1A = 250*TICK;
+
+	// CTC mode (clear timer on compare match
+	TCCR1B |= (1 << WGM12);
+
+	// 64 prescaler
+	TCCR1B |= (1 << CS11);
+	TCCR1B |= (1 << CS10);
+
+	// enable timer compare interrupt
+	TIMSK1 |= (1 << OCIE1A);
+
+	interrupts();
+
+	update_display();
 }
 
-void loop() {
-	// TODO sleep
+void loop () {
+	// standby mode with timer1 active and CPU off
+	SMCR |= 0b1101;
 }
 
 // Interrupt service routine, scheduler
-void ISR(void) {
+ISR(TIMER1_COMPA_vect) {
 	if (heartbeat_count++ == HEARTBEAT_PERIOD) {
 		heartbeat_count = 0;
 		inc_time();
@@ -42,16 +74,20 @@ void ISR(void) {
 
 // call at 1Hz
 void inc_time (void) {
-	if (time.second++ == 60) {
-		time.minute++;
-		time.second = 0;
+	if (time.seconds++ == 60) {
+		time.minutes++;
+		time.seconds = 0;
 	}
 
-	if (time.minute++ == 60) {
-		time.hour++;
-		time.minute = 0;
+	if (time.minutes++ == 60) {
+		time.hours++;
+		time.minutes = 0;
 	}
 
-	if (time.hour == 24)
-		time.hour = 0;
+	if (time.hours == 24)
+		time.hours = 0;
+}
+
+void update_display (void) {
+
 }
