e0ad9f849ed2495872130ef02f80d839c3cbc016
Callan Bryant <callan.bryant@gmail.com>
2012-12-19 18:11:57 +0000

    SPI control for the display

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,13 +1,23 @@
 // TODO general scheduler addTask(pointer,interval,offset)
 // and better with a queue and priorities?
 
+// TODO feed-forward brightness adjusting based on LDR value. 2 values is fine.
+// TODO debounced (async) buttons for setting time
+// TODO alarm?
+// TODO split into headers (and defines for SPI commands)
+// TODO use registers instead of digitalwrite, etc
+
 #include "current_time.h"
+#include <SPI.h>
 
 // tick interval in ms, 1-20 recommended, 260 max
 #define TICK 20
 
 // 1 second intervals to update clock
 #define HEARTBEAT_PERIOD (1000/TICK)
+
+// display SPI slave select pin
+#define DISP_SS 10
 
 volatile int heartbeat_count = 0;
 
@@ -18,8 +28,10 @@ typedef struct {
 	char seconds;
 } elapsed;
 
-// initialise with compile-time time
-elapsed time = {HOURS,MINUTES,SECONDS};
+// initialise with compile-time time with an offset to account for buildi/upload time
+elapsed time = {HOURS,MINUTES,SECONDS+4};
+
+void init_display(void);
 
 // tasks
 void inc_time (void);
@@ -54,7 +66,7 @@ void setup () {
 
 	interrupts();
 
-	update_display();
+	init_display();
 }
 
 void loop () {
@@ -89,5 +101,38 @@ void inc_time (void) {
 }
 
 void update_display (void) {
+	// select dsiplay
+	digitalWrite(DISP_SS,0);
 
+	// reset display, just in case the cursor has been moved
+	SPI.transfer(0x77);
+
+	// hours
+	SPI.transfer(time.hours%10);
+	SPI.transfer(time.hours/10);
+
+	// minutes
+	SPI.transfer(time.minutes%10);
+	SPI.transfer(time.minutes/10);
+
+	// deselect display
+	digitalWrite(DISP_SS,1);
+}
+
+void init_display(void) {
+	// init SPI
+
+	// slave select pin init
+	pinMode (DISP_SS, OUTPUT);
+	digitalWrite(DISP_SS,1);
+
+	// initialize SPI:
+	SPI.begin();
+
+	// turn on colon
+	SPI.transfer(0x77);
+	SPI.transfer(0x08);
+
+	// fill with initial time
+	update_display();
 }
