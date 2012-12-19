c86ab44c8e8ca423e4deca963f53a3dc63c3e8c5
Callan Bryant <callan.bryant@gmail.com>
2012-12-19 14:41:51 +0000

    basic operation, no hardware atm

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,9 +1,57 @@
 #include "current_time.h"
 
-void setup()
-{
+
+// tick interval in ms
+#define TICK 20
+
+// 1 second intervals to update clock
+#define HEARTBEAT_PERIOD (1000/TICK)
+
+int heartbeat_count = 0;
+
+
+typedef struct {
+	char hour    = HOURS;
+	char minute  = MINUTES;
+	char seconds = SECONDS;
+} time;
+
+volatile time time;
+
+void ISR (void);
+void inc_time (void);
+
+
+void setup() {
+	// TODO set up timer to call ISR every TICK ms
 }
 
-void loop()
-{
+void loop() {
+	// TODO sleep
+}
+
+// Interrupt service routine, scheduler
+void ISR(void) {
+	if (heartbeat_count++ == HEARTBEAT_PERIOD) {
+		heartbeat_count = 0;
+		inc_time();
+	}
+
+	// TODO: reset interrupt flag!
+}
+
+// call at 1Hz
+void inc_time (void) {
+	if (time.second++ == 60) {
+		time.minute++;
+		time.second = 0;
+	}
+
+	if (time.minute++ == 60) {
+		time.hour++;
+		time.minute = 0;
+	}
+
+	if (time.hour == 24)
+		time.hour = 0;
 }
